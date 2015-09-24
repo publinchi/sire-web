e8b08c750e06f97a4731a5da10db432fa4527550
publito <publinchi@gmail.com>
2015-09-24 05:09:14 +0000

    Agrego nuevos servicios Resful

---
+++ SIRE/SIRE-WS/src/main/java/com/sire/ws/service/InvArticuloFacadeREST.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sire.ws.service;
+
+import com.sire.entities.InvArticulo;
+import com.sire.entities.InvArticuloPK;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.PathSegment;
+
+/**
+ *
+ * @author Administrator
+ */
+@Stateless
+@Path("com.sire.entities.invarticulo")
+public class InvArticuloFacadeREST extends AbstractFacade<InvArticulo> {
+    @PersistenceContext(unitName = "com.sire_SIRE-WS_war_1.0.0PU")
+    private EntityManager em;
+
+    private InvArticuloPK getPrimaryKey(PathSegment pathSegment) {
+        /*
+         * pathSemgent represents a URI path segment and any associated matrix parameters.
+         * URI path part is supposed to be in form of 'somePath;codEmpresa=codEmpresaValue;codArticulo=codArticuloValue'.
+         * Here 'somePath' is a result of getPath() method invocation and
+         * it is ignored in the following code.
+         * Matrix parameters are used as field names to build a primary key instance.
+         */
+        com.sire.entities.InvArticuloPK key = new com.sire.entities.InvArticuloPK();
+        javax.ws.rs.core.MultivaluedMap<String, String> map = pathSegment.getMatrixParameters();
+        java.util.List<String> codEmpresa = map.get("codEmpresa");
+        if (codEmpresa != null && !codEmpresa.isEmpty()) {
+            key.setCodEmpresa(codEmpresa.get(0));
+        }
+        java.util.List<String> codArticulo = map.get("codArticulo");
+        if (codArticulo != null && !codArticulo.isEmpty()) {
+            key.setCodArticulo(new java.lang.Integer(codArticulo.get(0)));
+        }
+        return key;
+    }
+
+    public InvArticuloFacadeREST() {
+        super(InvArticulo.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void create(InvArticulo entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({"application/xml", "application/json"})
+    public void edit(@PathParam("id") PathSegment id, InvArticulo entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") PathSegment id) {
+        com.sire.entities.InvArticuloPK key = getPrimaryKey(id);
+        super.remove(super.find(key));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public InvArticulo find(@PathParam("id") PathSegment id) {
+        com.sire.entities.InvArticuloPK key = getPrimaryKey(id);
+        return super.find(key);
+    }
+
+    @GET
+    @Override
+    @Produces({"application/xml", "application/json"})
+    public List<InvArticulo> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<InvArticulo> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("/findByNombreArticulo/{nombreArticulo}")
+    @Produces({"application/json"})
+    public List<InvArticulo> findByNombreArticulo(@PathParam("nombreArticulo") String nombreArticulo) {
+        TypedQuery<InvArticulo> query = em.createNamedQuery("InvArticulo.findByNombreArticulo", InvArticulo.class);
+        query.setParameter("nombreArticulo", "%" + nombreArticulo + "%");
+        List<InvArticulo> retorno = query.getResultList();
+        return retorno;
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
