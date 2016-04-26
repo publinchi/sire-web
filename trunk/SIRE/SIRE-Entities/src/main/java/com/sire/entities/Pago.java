e830353e5d94607d07345c887ea217bf560bd813
publito <publito@e01b0fc6-4dad-4cc5-b8de-f9b2fed40558>
2016-04-26 17:50:38 +0000

    Se agrega entidades para el manejo de los pagos

--- trunk/SIRE/SIRE-Entities/src/main/java/com/sire/entities/Pago.java
+++ trunk/SIRE/SIRE-Entities/src/main/java/com/sire/entities/Pago.java
@@ -6,48 +6,21 @@
 package com.sire.entities;
 
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  *
  * @author publio
  */
+@Getter
+@Setter
 public class Pago {
 
     private List<CxcDocCobrar> cxcDocCobrarList;
     private CxcAbonoC cxcAbonoC;
     private CxcPagoContado cxcPagoContado;
     private List<CxcCheque> cxcChequeList;
-
-    public List<CxcDocCobrar> getCxcDocCobrarList() {
-        return cxcDocCobrarList;
-    }
-
-    public void setCxcDocCobrarList(List<CxcDocCobrar> cxcDocCobrarList) {
-        this.cxcDocCobrarList = cxcDocCobrarList;
-    }
-
-    public CxcAbonoC getCxcAbonoC() {
-        return cxcAbonoC;
-    }
-
-    public void setCxcAbonoC(CxcAbonoC cxcAbonoC) {
-        this.cxcAbonoC = cxcAbonoC;
-    }
-
-    public CxcPagoContado getCxcPagoContado() {
-        return cxcPagoContado;
-    }
-
-    public void setCxcPagoContado(CxcPagoContado cxcPagoContado) {
-        this.cxcPagoContado = cxcPagoContado;
-    }
-
-    public List<CxcCheque> getCxcChequeList() {
-        return cxcChequeList;
-    }
-
-    public void setCxcChequeList(List<CxcCheque> cxcChequeList) {
-        this.cxcChequeList = cxcChequeList;
-    }
+    private GnrLogHistorico gnrLogHistorico;
 
 }
