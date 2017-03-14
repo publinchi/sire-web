46c8f9637675c5ca24ecd8bfd701241d3ea023f2
publinchi <publinchi@gmail.com>
2017-03-14 14:33:03 -0500

    Update LazyInvArticuloDataModel.java

--- SIRE/SIRE-Web/src/main/java/com/sire/datamodel/lazy/LazyInvArticuloDataModel.java
+++ SIRE/SIRE-Web/src/main/java/com/sire/datamodel/lazy/LazyInvArticuloDataModel.java
@@ -36,5 +36,59 @@ public class LazyInvArticuloDataModel extends LazyDataModel<InvArticulo> {
         return invArticulo.getInvArticuloPK().getCodArticulo();
     }
     
-   
+    @Override
+    public List<InvArticulo> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String,Object> filters) {
+        List<InvArticulo> data = new ArrayList<InvArticulo>();
+ 
+        //filter
+        for(InvArticulo invArticulo : datasource) {
+            boolean match = true;
+ 
+            if (filters != null) {
+                for (Iterator<String> it = filters.keySet().iterator(); it.hasNext();) {
+                    try {
+                        String filterProperty = it.next();
+                        Object filterValue = filters.get(filterProperty);
+                        String fieldValue = String.valueOf(car.getClass().getField(filterProperty).get(car));
+ 
+                        if(filterValue == null || fieldValue.startsWith(filterValue.toString())) {
+                            match = true;
+                    }
+                    else {
+                            match = false;
+                            break;
+                        }
+                    } catch(Exception e) {
+                        match = false;
+                    }
+                }
+            }
+ 
+            if(match) {
+                data.add(invArticulo);
+            }
+        }
+ 
+        //sort
+        if(sortField != null) {
+            Collections.sort(data, new LazySorter(sortField, sortOrder));
+        }
+ 
+        //rowCount
+        int dataSize = data.size();
+        this.setRowCount(dataSize);
+ 
+        //paginate
+        if(dataSize > pageSize) {
+            try {
+                return data.subList(first, first + pageSize);
+            }
+            catch(IndexOutOfBoundsException e) {
+                return data.subList(first, first + (dataSize % pageSize));
+            }
+        }
+        else {
+            return data;
+        }
+    }
 }
