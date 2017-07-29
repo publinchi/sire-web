aeb72afe5c6702c2ec0c9af092558bdfd3366bc9
Publio Estupiñán <publinchi@gmail.com>
2017-07-29 12:20:24 -0500

    Se agrega log para validar que pasa con el cheque

--- SIRE/SIRE-Web/src/main/java/com/sire/web/CxcDocCobrarBean.java
+++ SIRE/SIRE-Web/src/main/java/com/sire/web/CxcDocCobrarBean.java
@@ -385,6 +385,10 @@ public class CxcDocCobrarBean {
     public void calcularCheques() {
         LOGGER.info("calcularCheques()");
         if (valorCheque > 0.0) {
+            LOGGER.info("totalCapital: " + totalCapital + ", retencion: " + retencion
+                    + ", retencionIVA: " + retencionIVA + ", efectivo: " + efectivo
+                    + ", deposito: " + deposito + ", otros: " + otros
+                    + ", totalCheques:" + totalCheques + ", valorCheque: " + valorCheque);
             Double dif = totalCapital - retencion - retencionIVA - efectivo - deposito - otros - totalCheques - valorCheque;
             dif = Round.round(dif, 2);
             LOGGER.log(Level.INFO, "dif: {0}", dif);
