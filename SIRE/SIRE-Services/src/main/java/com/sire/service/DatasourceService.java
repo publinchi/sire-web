e1e50792023dbc5b1c000a8bd445ad0123c88cbd
Publio Estupiñán <publinchi@gmail.com>
2018-10-28 12:03:08 -0500

    - Se implementa la obtención del nombre de vendor de BDD - Se mejora la conexión a la BDD

--- SIRE/SIRE-Services/src/main/java/com/sire/service/DatasourceService.java
+++ SIRE/SIRE-Services/src/main/java/com/sire/service/DatasourceService.java
@@ -6,13 +6,13 @@
 package com.sire.service;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import com.sire.service.IDatasourceService;
 
 /**
  *
@@ -22,19 +22,38 @@ import com.sire.service.IDatasourceService;
 @Startup
 public class DatasourceService implements IDatasourceService {
 
-    private Connection connection;
+    private String databaseProductName;
 
     @Override
     public Connection getConnection() throws SQLException, NamingException {
-        if (connection == null || (connection != null && connection.isClosed())) {
-            Context initContext = new InitialContext();
-            String datasource = System.getProperty("sire.datasource");
-            if (datasource == null) {
-                datasource = "jdbc/sire";
+        return _getConnection(-1);
+    }
+
+    @Override
+    public Connection getConnection(int timeout) throws SQLException, NamingException {
+        return _getConnection(timeout);
+    }
+
+    @Override
+    public String getDatabaseProductName() throws SQLException, NamingException {
+        if(databaseProductName == null){
+            try(Connection connection = getConnection()) {
+                DatabaseMetaData databaseMetaData = connection.getMetaData();
+                databaseProductName = databaseMetaData.getDatabaseProductName();
             }
-            javax.sql.DataSource ds = (javax.sql.DataSource) initContext.lookup(datasource);
-            connection = ds.getConnection();
         }
-        return connection;
+        return databaseProductName;
+    }
+
+    private Connection _getConnection(int timeout) throws SQLException, NamingException {
+        Context initContext = new InitialContext();
+        String datasource = System.getProperty("sire.datasource");
+        if (datasource == null) {
+            datasource = "jdbc/sire";
+        }
+        javax.sql.DataSource ds = (javax.sql.DataSource) initContext.lookup(datasource);
+        if(timeout !=-1)
+            ds.setLoginTimeout(timeout);
+        return ds.getConnection();
     }
 }
