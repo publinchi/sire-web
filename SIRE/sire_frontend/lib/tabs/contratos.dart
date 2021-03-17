bd6c81e1a0a9c1f0be0c25dea51c8defdf9aa423
kevin <kevinsevillano38@gmail.com>
2021-03-17 17:47:11 -0500

    Se cambia el nombre de clase

--- SIRE/sire_frontend/lib/tabs/contratos.dart
+++ SIRE/sire_frontend/lib/tabs/contratos.dart
@@ -24,7 +24,7 @@ class ContratosView extends StatelessWidget {
 
     return TabWithSidebar(
       restorationId: 'accounts_view',
-      mainView: FinancialEntityView(
+      mainView: FinancialCuotaView(
         heroLabel: GalleryLocalizations.of(context).rallyAccountTotal,
         heroAmount: balanceTotal,
         segments: buildSegmentsFromContratoItems(items),
